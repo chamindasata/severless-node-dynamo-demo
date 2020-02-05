380a84bf0bb97a7262f287f89863863b0e08169d
chamindasata <45987746+chamindasata@users.noreply.github.com>
2020-02-06 04:18:50 +0530

    change header

--- todos/list.js
+++ todos/list.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
-const AWS = require('aws-sdk'); 
+const AWS = require("aws-sdk");
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const params = {
-  TableName: process.env.DYNAMODB_TABLE,
+  TableName: process.env.DYNAMODB_TABLE
 };
 
 module.exports.list = (event, context, callback) => {
@@ -15,15 +15,18 @@ module.exports.list = (event, context, callback) => {
       console.error(error);
       callback(null, {
         statusCode: error.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t fetch the todos.',
+        headers: { "Content-Type": "text/plain" },
+        body: "Couldn't fetch the todos."
       });
       return;
     }
     // create a response
     const response = {
       statusCode: 200,
-      body: JSON.stringify(result.Items),
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify(result.Items)
     };
     callback(null, response);
   });
